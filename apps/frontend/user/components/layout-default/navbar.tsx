91ed5477c1c2e0f7390dcb77d3d42e78d0a6d6ca
Rio Chandra <riochandra4@gmail.com>
2022-08-13 13:48:28 +0700

    fix: navbar auth

--- apps/frontend/user/components/layout-default/navbar.tsx
+++ apps/frontend/user/components/layout-default/navbar.tsx
@@ -1,17 +1,29 @@
-import { Box, Flex, Button, Text, Container, Avatar, Wrap, WrapItem, HStack, Menu, MenuButton, MenuList, MenuItem, Link, useBreakpointValue, useColorModeValue, ButtonGroup, IconButton, Drawer, useDisclosure, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, Stack } from "@chakra-ui/react";
+import { Box, Flex, Button, Text, Container, Avatar, Wrap, WrapItem, HStack, Menu, MenuButton, MenuList, MenuItem, Link, useBreakpointValue, useColorModeValue, ButtonGroup, IconButton, Drawer, useDisclosure, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, Stack, Divider, StackDirection } from "@chakra-ui/react";
 import Router from "next/router";
 import NextLink from 'next/link'
 import { useAuth } from "../../lib/auth/useAuth";
 import { FiMenu } from 'react-icons/fi'
 import React from "react";
 
+interface MenuItemInterface {
+  text: string
+  to?: string
+  onClick?: any
+}
+
 export default function Navbar() {
   const authContext = useAuth()
-  const Listmenu: {text: string, to: string}[] = [
+  const Listmenu:MenuItemInterface[] = [
     {text: "Home", to: '/home'},
     {text: "Contact", to: '/contact'},
     {text: "About", to: '/about'},
   ]
+
+  const AccountMenuList:MenuItemInterface[] = [
+    {text: 'Account', to: '/account'},
+    {text: 'Logout', onClick: () => authContext.logout()},
+  ]
+
   const ListMenuComponent = ({ isVertical = false }) => {
     return (
       <Stack spacing="8" direction={isVertical ? 'column' : 'row'}>
@@ -24,32 +36,95 @@ export default function Navbar() {
     )
   }
 
+  const ButtonAuth = ({ direction }: {direction: StackDirection}) => {
+    return (
+      <Stack spacing="3" direction={direction}>
+        <Button variant="ghost" onClick={() => Router.push('/login')}>Sign in</Button>
+        <Button variant="primary" onClick={() => Router.push('/login')}>Sign up</Button>
+      </Stack>
+    )
+  }
+
   const ProfileMenuComponent = () => {
     if(!authContext.user){
       return (
-        <HStack spacing="3">
-          <Button variant="ghost" onClick={() => Router.push('/login')}>Sign in</Button>
-          <Button variant="primary" onClick={() => Router.push('/login')}>Sign up</Button>
-        </HStack>
+        <ButtonAuth direction="row" />
       )
     }
+
+    const AccountMenuListComponent = () => {
+      const listMenu = AccountMenuList.map((menu, i) => {
+        let MenuItemProps:any = {}
+        if(menu.to){
+          MenuItemProps = {
+            onClick: () => Router.push(menu.to)
+          }
+        }
+        if(menu.onClick){
+          MenuItemProps = {
+            onClick : menu.onClick
+          }
+        }
+
+        return (
+          <MenuItem {...MenuItemProps} key={i}>{menu.text}</MenuItem>
+        )
+      })
+      return (
+        <MenuList>
+          {listMenu}
+        </MenuList>
+      )
+    }
+
     return (
       <Menu>
         <MenuButton>
           <Avatar name={authContext.user?.name} src={authContext.user?.picture} bg='primary' color='white' outline='1'/>
         </MenuButton>
-        <MenuList>
-          <MenuItem>Account</MenuItem>
-          <MenuItem onClick={() => authContext.logout()}>Logout</MenuItem>
-        </MenuList>
+        <AccountMenuListComponent />
       </Menu>
     )
   }
   
   const DrawerSide = ({ isDesktop }) => {
+    const { onOpen, isOpen, onClose } = useDisclosure()
     const buttonRef = React.useRef()
 
-    const { onOpen, isOpen, onClose } = useDisclosure()
+    const AccountComponent = () => {
+      if(!authContext.user){
+        return (<ButtonAuth direction="column" />)
+      }
+      const listMenuAccountComponent = () => (
+        AccountMenuList.map((menu, i) => {
+          const boxProps:any = {}
+          if(menu.to){
+            boxProps.onClick = () => Router.push(menu.to)
+          }
+          if(menu.onClick){
+            boxProps.onClick = menu.onClick
+          }
+          return (
+            <Box {...boxProps} padding='2' mt='6' cursor='pointer'>
+              <Text size='md' color='primary' fontWeight='bold'>
+                {menu.text}
+              </Text>
+            </Box>
+          )
+        })
+      )
+
+      return (
+        <div>
+          <Box display='flex' alignItems='center'>
+            <Avatar name={authContext.user?.name} src={authContext.user?.picture} bg='primary' color='white' outline='1' mr='3'/>
+            <Text size="2xl">{authContext.user?.name}</Text>
+          </Box>
+          {listMenuAccountComponent()}
+        </div>
+      )
+    }
+
     return (
       <>
         <IconButton
@@ -74,6 +149,8 @@ export default function Navbar() {
 
             <DrawerBody>
               <ListMenuComponent isVertical={true} />
+              <Divider my='6' />
+              <AccountComponent />
             </DrawerBody>
           </DrawerContent>
         </Drawer>
