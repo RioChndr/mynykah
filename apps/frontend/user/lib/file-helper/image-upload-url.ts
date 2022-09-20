636e8aa2fbcdce8c19bda25f163d1f2418bda35a
Rio Chandra <riochandra4@gmail.com>
2022-09-20 20:39:43 +0700

    fix: image url

--- apps/frontend/user/lib/file-helper/image-upload-url.ts
+++ apps/frontend/user/lib/file-helper/image-upload-url.ts
@@ -3,5 +3,7 @@ import { join } from "path"
 export const imageUploadUrl = (filePath: string) => {
   const prefix = process.env.DO_SPACES_SUBDOMAIN
   if (!prefix || !filePath) return filePath
-  return join(prefix, filePath)
+  const url = new URL(prefix)
+  url.pathname = join(url.pathname, filePath)
+  return url.toString();
 }
